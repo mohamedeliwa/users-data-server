2c3d73c9c18b3c72b80de414759d773ed420b06d
mohamedeliwa <mohamed.av11@gmail.com>
2020-12-13 22:57:26 +0200

    allowing cors for development

--- src/app.js
+++ src/app.js
@@ -1,17 +1,19 @@
 const express = require("express");
-
+const cors = require("cors");
 // Calling mongoose file to connect to the database
 require("./db/mongoose");
 // importing user router
 const userRouter = require("./routers/user");
 
-
 const app = express();
 
 // Configuring express to automatically parse json data
 app.use(express.json());
-
-
+/**
+ * Enabling CORS for local testing purposes.
+ * to be disabled on production
+ */
+app.options("*", cors());
 
 // Configuring express to use user router
 app.use(userRouter);
