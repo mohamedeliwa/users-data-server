8cc9e66712d8de85a18cd18cf093d1c39b4db979
mohamedeliwa <mohamed.av11@gmail.com>
2020-12-13 16:50:37 +0200

    created user model

---
+++ src/models/user.js
@@ -0,0 +1,53 @@
+const mongoose = require("mongoose");
+
+// Creating a mongoose Schema for User Model
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      trim: true,
+      minlength: 2,
+      maxlength: 50,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      trim: true,
+      lowercase: true,
+    },
+
+    gender: {
+      // True for male, False for Female
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  {
+    // adding timestamps to each user doucment
+    timestamps: true,
+  }
+);
+
+userSchema.methods.toJSON = function () {
+  const user = this;
+  const userObject = user.toObject();
+
+  //   delete userObject.reports;
+
+  return userObject;
+};
+
+// Creating a User model.
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
